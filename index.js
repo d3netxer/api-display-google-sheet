81d6ba60092228d7f687bb4c351c83f52442e054
d3netxer <tommygun320@yahoo.com>
2018-10-20 09:55:04 -0400

    added geocoder

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const {google} = require('googleapis');
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
 const TOKEN_PATH = 'token.json';
 
+const MAPBOX_ClientID_PATH = 'token.json';
 
+var geo = require('mapbox-geocoding');
 
 const serverless = require('serverless-http');
 const express = require('express')
@@ -37,8 +39,8 @@ function authorize(credentials, callback, req, res) {
     console.log('before callback');
     //console.log(res);
     callback(oAuth2Client,req,res);
-
   });
+
 }
 
 /**
@@ -72,13 +74,52 @@ function getNewToken(oAuth2Client, callback) {
   });
 }
 
+
+function geocode (location) {
+
+
+  return new Promise((resolve, reject) => {
+
+        console.log("inside geocode function");
+        console.log(location);
+        //console.log(country);
+
+        geo.geocode('mapbox.places', location, function (err, geoData) {
+                      //console.log(geoData.features[0].center[0]);
+                      //console.log(geoData.features[0].center[1]);
+
+                      //console.log('api result about to get pushed')
+                      //console.log(apiResult);
+                      lat =  geoData.features[0].center[0].toString() ;
+                      lon =  geoData.features[0].center[1].toString() ;
+                      //apiEntry.push("lon"  : geoData.features[0].center[1].toString());
+                      //console.log('apiEntry');
+                      //console.log(apiEntry);
+
+                      resolve([lat,lon]);
+
+                      //console.log('apiResult before push');
+                      //console.log(apiResult);
+
+                      //apiResult.push(apiEntry);
+
+                      
+          });
+
+
+
+    })
+
+
+
+}
+
+
 /**
  * Prints the names and majors of students in a sample spreadsheet:
  * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
  * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
  */
-
-
 
 function listMajors(auth,req,res) {
 
@@ -88,40 +129,45 @@ function listMajors(auth,req,res) {
     spreadsheetId: '1bCgCA-YJxcH9SVkPIEtHnjaIZaAZgnSaf9epuHkfmF4',
     range: 'A2:Q',
   }, (err, result) => {
-    if (err) return console.log('The API returned an error: ' + err);
+      if (err) return console.log('The API returned an error: ' + err);
 
-    const rows = result.data.values;
+      const rows = result.data.values;
 
-    const apiResult = [];
+      var apiResult = [];
 
-    if (rows.length) {
+      if (rows.length) {
 
-      console.log('Name, Major:');
-      // Print columns A and E, which correspond to indices 0 and 4.
+        //console.log('Name, Major:');
+        // Print columns A and E, which correspond to indices 0 and 4.
 
-      rows.map((row) => {
+        //keep only rows that are part of OSMGeoWeek
+        var geoweekRows = [] 
 
-          //column index for is your event part of osmgeoweek is 15
-          //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
-          var templateLiteralString = row[15];
-
-          //console.log('print type templateLiteralString');
-          //console.log(typeof templateLiteralString);
-          //console.log(templateLiteralString);
-
-          if (templateLiteralString) {  
-
-              //var n = templateLiteralString.localeCompare("yes");
-              //console.log('print n');
-              //console.log(n);
-
+        var i;
+        for (i = 0; i < rows.length; i++) { 
+            var templateLiteralString = rows[i][15];
+            if (templateLiteralString) {  
               if (templateLiteralString == "Yes") { 
-                
-                //console.log('templateLiteralString equals yes');
-                //console.log(templateLiteralString);
-                //console.log(`${row[0]}, ${row[15]}`);
+                geoweekRows.push(rows[i]);
+              }
+            }
+        }
 
-                apiResult.push({ 
+        console.log('print geoweekRows length');
+        console.log(geoweekRows.length);
+
+        let promises = geoweekRows.map(row => {
+          return geocode(`${row[4]},${row[5]}`)
+          //return geocode("sterling","virginia")
+            .then(function(result) {
+              console.log('print result');
+              console.log(result);
+              //console.log(lat);
+              //console.log(lon);
+              //return 'lat';
+
+
+              var apiEntry = {
                     "timestamp" : `${row[0]}`,
                     "org_name"  : `${row[3]}`,
                     "location"  : `${row[4]}`,
@@ -132,33 +178,53 @@ function listMajors(auth,req,res) {
                     "sign_up_link"  : `${row[10]}`,
                     "venue_name"  : `${row[12]}`,
                     "osm_link"  : `${row[13]}`,
-                    "mapping_party_name"  : `${row[14]}`
-                });
+                    "mapping_party_name"  : `${row[14]}`,
+                    "lat"  : result[1].toString(),
+                    "lon"  : result[0].toString()
+                }
 
-              }
-          }
-        
+              //console.log('print apiEntry');
+              //console.log(apiEntry);
+
+              apiResult.push(apiEntry);
+
+
+            })
+
+
+
       });
 
-      console.log('apiResult');
-      console.log(apiResult);
 
-      //console.log('print rows');
-      //console.log(rows);
+        // Wait for all Promises to complete
+        Promise.all(promises)
+          .then(results => {
+            // Handle results
+            console.log('apiResult after Promise all');
+            console.log(apiResult);
 
-      //res.send('get events!');
+            res.json(apiResult);
+          })
+          .catch(e => {
+            console.error(e);
+          })
 
-      res.json(apiResult);
+      //console.log('geocoder test');
+      // Geocode an address to coordinates
+      /*
+      geo.geocode('mapbox.places', 'Dam Square, Amsterdam', function (err, geoData) {
+        console.log(geoData.features[0].center);
+      });
+      */
 
-
+      
 
     } else {
       console.log('No data found.');
     }
   });
+
 }
-
-
 
 
 app.get('/', function (req, res) {
@@ -167,6 +233,17 @@ app.get('/', function (req, res) {
 
 
 app.get('/events/', function (req,res) {
+
+  //load MapBox access token
+  fs.readFile('mapbox_client_id.json', (err, content) => {
+    if (err) return console.log('need a config.json file with MapBox ClientID Token');
+    //console.log('print content');
+    //console.log(JSON.parse(content).access_token);
+    var mapbox_access_token = JSON.parse(content).access_token;
+    geo.setAccessToken(mapbox_access_token);
+    console.log('loaded MapBox ClientID Token');
+  });
+
   // Load client secrets from a local file.
   //https://stackoverflow.com/questions/10058814/get-data-from-fs-readfile
   //function you have defined is an asynchronous callback. It doesn't execute right away, rather it executes when the file loading has completed. When you call readFile, control is returned immediately and the next line of code is executed.
@@ -175,6 +252,7 @@ app.get('/events/', function (req,res) {
     // Authorize a client with credentials, then call the Google Sheets API.
     authorize(JSON.parse(content), listMajors, req, res);
   });
+
 })
 
 module.exports.handler = serverless(app);
